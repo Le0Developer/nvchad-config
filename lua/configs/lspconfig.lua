4ced94e7d8096af426fd5f7b80c9f33651b428b6
Leo <leodev@mac.speedport.ip>
2024-11-20 18:58:30 +0100

    add some more LSPs

--- lua/configs/lspconfig.lua
+++ lua/configs/lspconfig.lua
@@ -4,7 +4,7 @@ require("nvchad.configs.lspconfig").defaults()
 local lspconfig = require "lspconfig"
 
 -- EXAMPLE
-local servers = { "html", "cssls" }
+local servers = { "html", "cssls", "gopls", "pyright", "ts_ls", "tailwindcss-language-server" }
 local nvlsp = require "nvchad.configs.lspconfig"
 
 -- lsps with default config
