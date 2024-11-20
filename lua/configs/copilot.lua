93463af52dfc5e23e95f7f43c6da7745dbbc0271
Leo Developer <git@leodev.win>
2024-11-20 21:21:01 +0100

    fix copilot appending garbage when accepting suggestion

--- lua/configs/copilot.lua
+++ lua/configs/copilot.lua
@@ -3,11 +3,8 @@
 vim.g.copilot_assume_mapped = true
 vim.g.copilot_no_tab_map = true
 
-vim.keymap.set("i", "<S-Enter>", "copilot#Accept(\"\")", {
-  silent = true,
-  expr = true,
-  desc = "Autocomplete using Github Copilot"
-})
+-- if someone knows a way to do this using the regular keymap API, please let me know
+vim.cmd[[inoremap <silent><expr> <S-Enter> copilot#Accept("")]]
 
 -- Allow completion in additional filetypes:
 vim.g.copilot_filetypes = {
