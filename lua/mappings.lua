8c4f65a4279b57d0f9dbdb0522c3dc0f18e25d6b
Leo <leodev@mac.speedport.ip>
2024-11-20 18:58:18 +0100

    add <leader>X mapping to close all buffers

--- lua/mappings.lua
+++ lua/mappings.lua
@@ -6,5 +6,8 @@ local map = vim.keymap.set
 
 map("n", ";", ":", { desc = "CMD enter command mode" })
 map("i", "jk", "<ESC>")
+map("n", "<leader>X", function ()
+  require("nvchad.tabufline").closeAllBufs()
+end, { desc = "Close all buffers" })
 
 -- map({ "n", "i", "v" }, "<C-s>", "<cmd> w <cr>")
