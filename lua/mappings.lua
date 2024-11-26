4ea22fd358a345f4e8f9be838708eeffa2de853c
Leo Developer <git@leodev.win>
2024-11-26 13:45:09 +0100

    add <leader>lf bind back!

--- lua/mappings.lua
+++ lua/mappings.lua
@@ -10,4 +10,8 @@ map("n", "<leader>X", function ()
   require("nvchad.tabufline").closeAllBufs()
 end, { desc = "Close all buffers" })
 
+map("n", "<leader>lf", function ()
+  vim.diagnostic.open_float({ border = "rounded" })
+end, { desc = "Floating diagnostics" })
+
 -- map({ "n", "i", "v" }, "<C-s>", "<cmd> w <cr>")
